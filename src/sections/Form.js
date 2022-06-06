12cde248cc7f9d59739b65ad7f2a895a730a64ec
Letícia Medeiros Nascimento <leticiamedeirosnasc@gmail.com>
2022-06-06 19:44:08 -0300

    Form

--- src/sections/Form.js
+++ src/sections/Form.js
@@ -4,7 +4,13 @@ class Form extends Component {
     render() {
         return (
             <div>
-                
+                <form>
+                    <h2>Compre ou alugue</h2>
+                    <input type="text" name="name" id="name" placeholder="Seu nome completo"/>
+                    <input type="text" name="phone" id="phone" placeholder="Seu telefone (WhatsApp)"/>
+                    <input type="email" name="email" id="email" placeholder="Seu melhor e-mail" />
+                    <input type="tel" value="ENCONTRE SEU IMÓVEL" />
+                </form>
             </div>
         );
     }
