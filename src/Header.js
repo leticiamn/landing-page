60a3399dbc8e509134ad504cda0c4e7cd06b0fc7
Letícia Medeiros Nascimento <leticiamedeirosnasc@gmail.com>
2022-06-20 00:45:34 -0300

    ajustes

--- src/Header.js
+++ src/Header.js
@@ -10,9 +10,9 @@ class Header extends Component {
                         <img src="./img/logo.png" alt="Camilla Rocha" />
                     </div>
                     <div class="social">
-                        <a href=""><img src="./img/icons/instagram.png" alt="Instagram" /></a>
-                        <a href=""><img src="./img/icons/facebook.png" alt="Facebook" /></a>
-                        <a href=""><img src="./img/icons/linkedin.png" alt="LinkedIn" /></a>
+                        <a href="#"><img src="./img/icons/instagram.png" alt="Instagram" /></a>
+                        <a href="#"><img src="./img/icons/facebook.png" alt="Facebook" /></a>
+                        <a href="#"><img src="./img/icons/linkedin.png" alt="LinkedIn" /></a>
                     </div>
                 </div>
             </header>
