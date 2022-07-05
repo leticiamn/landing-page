b2edf992b79880aa3de83ffde5590ba94a657cc6
Letícia Medeiros Nascimento <leticiamedeirosnasc@gmail.com>
2022-07-04 23:09:27 -0300

    validando formulario

--- src/sections/Form.js
+++ src/sections/Form.js
@@ -1,78 +1,108 @@
 import { useState } from "react";
+import { useCheckbox, useForm, useInput } from "lx-react-form";
 import { Link, Navigate } from 'react-router-dom';
-import validator from 'validator';
 
-function Form() {
-    const[user,setUser] = useState({nome:'', telefone:'', email:''});
+const Form = () => {
+
     const [submitted, setSubmitted] = useState(false);
-    const [emailVerified, setEmailVerified] = useState();
-    const [termsVerified, setTermsVerified] = useState();
-    const [nameVerified, setNameVerified] = useState();
-    const [phoneVerified, setPhoneVerified] = useState();
 
-    const getData = (event) => {
-        const { name, value, checked } = event.target;
-        if (name == 'terms') {           
-            if (checked) setTermsVerified(true);
-            else setTermsVerified(false);
+    const name = useInput({
+        name: "nome",
+        errorText: {
+            required: "Este campo é obrigatório"
         }
-        else
-            setUser({ ...user, [name]: value });
-        
+    });
+
+    const email = useInput({
+        name: "email",
+        validation: "email",
+        errorText: {
+            required: "Este campo é obrigatório"
+        }
+    });
+
+    const phone = useInput({
+        name: "telefone",
+        validation: "telefone",
+        mask: "telefone",
+        errorText: {
+            required: "Este campo é obrigatório"
+        }
+    });
+
+    const terms = useCheckbox({
+        name: "terms",
+        errorText: {
+            required: "O aceite aos termos é obrigatório para continuar"
+        }
+    });
+
+    const form = useForm({
+        clearFields: true,
+        formFields: [name, email, phone, terms],
+        submitCallback: (formData) => {
+            sendForm(formData);
+        },
+    });
+
+    const sendForm = (form) => {
+        const newForm = {
+            "nome": form.nome,
+            "email": form.email,
+            "telefone": form.telefone.replace(/[^0-9]+/g, '')
+        }
+        console.log(newForm);
+        fetch('https://projetocamilla.herokuapp.com/form/cadastro', {
+            method: 'POST',
+            body: JSON.stringify(newForm),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(function (response) {
+                if (response.ok) setSubmitted(true);
+            })
+            .catch(function (err) { console.error(err); });
     }
 
-    const verify = (event) => {
-        if (validator.isEmail(user.email)) setEmailVerified(true);
-        else setEmailVerified(false);
-        if (validator.isMobilePhone(user.telefone, 'pt-BR')) setPhoneVerified(true);
-        else setPhoneVerified(false);
-        if (!validator.isEmpty(user.nome)) setNameVerified(true);
-        else setNameVerified(false);
-
-        if (emailVerified && nameVerified && phoneVerified && termsVerified)
-            fetch('https://projetocamilla.herokuapp.com/form/cadastro', {
-                method: 'POST',
-                body: JSON.stringify(user),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-                .then(function (response) {
-                    if (response.ok) setSubmitted(true);
-                });
-        else alert('Error');
-    
+    function preventDefault(e) {
+        e.preventDefault();
+        form.handleSubmit(e);
     }
 
     return (
         <div>
             {submitted ? (
-                <Navigate to = "/thankyou" replace = "true"/>
+                <Navigate to="/thankyou" replace="true" />
             ) : (
-                    
-            <div  id="form" className="form" name="form">
-                <form>
-                    <h2>Encontre seu imóvel</h2>
-                            <input type="text" name="nome" id="nome" placeholder="Seu nome completo" onChange={getData} />
-                    
-                    <input type="tel" name="telefone" id="telefone" placeholder="Seu telefone (WhatsApp)" minLength="8" onChange={getData}/>
-                    
-                    <input type="email" name="email" id="email" placeholder="Seu melhor e-mail" onChange={getData} />
-                    
-                    <label>
-                        <input type="checkbox" name="terms" id="terms" value="Terms" onChange={getData}/>
-                        Li e aceito os <Link to= "/terms"> termos e condições</Link>.
-                    </label>
-                    
-                    <input type="button" value="ENTRE EM CONTATO" onClick={verify} />
-                        
-                </form>
-            </div>
-            )}
-        
-        </div>
+
+                <div id="form" className="form" name="form">
+                    <form onSubmit={preventDefault}>
+                        <h2>Encontre seu imóvel</h2>
+
+                        <input type="text" {...name.inputProps} />
+                        {name.error && <p>{name.error}</p>}
+
+                        <input type="email" {...email.inputProps} />
+                        {email.error && <p>{email.error}</p>}
+
+                        <input type="tel" {...phone.inputProps} />
+                        {phone.error && <p>{phone.error}</p>}
+
+                        <label htmlFor={terms.name}>
+                            <input type="checkbox" {...terms.inputProps} />
+                            <span>Eu aceito os <Link to="/terms">termos e condições</Link>.</span>
+                        </label>
+                        {terms.error && <p>{terms.error}</p>}
+                        <button button type="submit" >ENTRE EM CONTATO</button>
+                    </form>
+                </div>
+            )
+            }
+
+        </div >
     );
-    
+
 }
 
 export default Form;
