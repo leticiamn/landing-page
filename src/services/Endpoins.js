b01065ae57c2ab05b88b3e19aceb4817cabe3aba
Letícia Medeiros Nascimento <leticiamedeirosnasc@gmail.com>
2022-07-05 21:46:11 -0300

    edpoints

--- src/services/Endpoins.js
+++ src/services/Endpoins.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://projetocamilla.herokuapp.com/",
   headers: {
     "Content-type": "application/json",
   },
